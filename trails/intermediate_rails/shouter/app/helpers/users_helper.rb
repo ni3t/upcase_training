22078a515a87ae55be7cd9938a7fba6c37b461cc
ni3t <bell.nicholas.p@gmail.com>
2016-06-21 23:59:45 -0400

    modified readme and app

---
+++ trails/intermediate_rails/shouter/app/helpers/users_helper.rb
@@ -0,0 +1,6 @@
+module UsersHelper
+	def follow_button user
+		button_to 'Follow', user_follow_path(user)
+	end
+end
+
--- trails/intermediate_rails/shouter/config/routes.rb
+++ trails/intermediate_rails/shouter/config/routes.rb
@@ -2,12 +2,17 @@ Shouter::Application.routes.draw do
 	
 	root to: 'homes#show', via: :get
 
-	resources :shouts, only: [:create, :show]
+	resources :shouts, only: [ :show]
+
+	resources :text_shouts, only: [:create]
+
+	resources :photo_shouts, only: [:create]
 
 	resource :dashboard, only: [:show]
 
 	resource :session, only: [:new, :create, :destroy]
 
-	resources :users, only: [:new, :create, :show]
-
+	resources :users, only: [:new, :create, :show] do
+		post 'follow' => 'following_relationships#create'
+	end
 end
